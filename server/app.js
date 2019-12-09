51ee23c30bdb357b95eb236f8d27bedabec5491c
hzoou <woojooo52@naver.com>
2019-12-09 21:36:21 +0900

    chore: 인덴트 수정
    app.js 파일의 인덴트를 수정했습니다.

--- server/app.js
+++ server/app.js
@@ -3,16 +3,16 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 io.on("connection", socket => {
-  socket.emit("connectted");
-  socket.on("joinRoom", room => {
-    socket.join(room);
-    console.log(`${socket.id}: join room ${room}`);
-  });
-  socket.on("change", (event, value) => {
-    socket.broadcast.emit("change", event, value);
-  });
+    socket.emit("connectted");
+    socket.on("joinRoom", room => {
+        socket.join(room);
+        console.log(`${socket.id}: join room ${room}`);
+    });
+    socket.on("change", (event, value) => {
+        socket.broadcast.emit("change", event, value);
+    });
 });
 
 server.listen(3030, () => {
-  console.log("Socket IO server listening on port 3030");
+    console.log("Socket IO server listening on port 3030");
 });
