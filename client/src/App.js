b4f004f56d9469ba64319905a6727a1add579bd1
hzoou <woojooo52@naver.com>
2019-12-09 21:28:46 +0900

    feat: ot 알고리즘에 적용할 operation 파싱
    diff 알고리즘을 사용해 받아온 diffrenece를
    ot 알고리즘에 적용하기 위해 (일단)  대략적으로 파싱했습니다.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
+import * as Diff from 'diff';
 
 import Editor from "./Editor";
 
@@ -8,39 +9,52 @@ const endpoint = "http://127.0.0.1:3030";
 const roomName = "basiltoast";
 
 function App() {
-  const [code, setCode] = useState("");
-  const [eventMask, setEventMask] = useState(true);
+    const [code, setCode] = useState("hello");
+    const [eventMask, setEventMask] = useState(true);
 
-  const initSocketSetting = () => {
-    socket = io(endpoint);
-    socket.on("connectted", () => socket.emit("joinRoom", roomName));
-    socket.on("change", (id, value) => {
-      setEventMask(false);
-      setCode(value);
-    });
-  };
+    const initSocketSetting = () => {
+        socket = io(endpoint);
+        socket.on("connectted", () => socket.emit("joinRoom", roomName));
+        socket.on("change", (id, value) => {
+            setEventMask(false);
+            setCode(value);
+        });
+    };
 
-  const maskingEvent = () => {
-    if (eventMask) {
-      socket.emit("change", socket.id, code);
-      setEventMask(false);
-    }
-  };
+    const maskingEvent = () => {
+        if (eventMask) {
+            socket.emit("change", socket.id, code);
+            setEventMask(false);
+        }
+    };
 
-  const handleOnChange = (event, value) => {
-    setEventMask(true);
-    setCode(value);
-  };
+    const handleOnChange = (event, value) => {
+        const diff = Diff.diffChars(code, value);
+        const history = [];
+        diff.forEach(({ count, added, removed, value }) => {
+            if (!added && !removed) { //retain
+                history.push(`retain(${count})`);
+            } else if (added) { //insert
+                history.push(`insert('${value}')`);
+            } else if (removed) { //delete
+                history.push(`delete(${count})`);
+            }
+            console.log(count, added, removed, value);
+        });
+        console.log('history :', history);
+        setEventMask(true);
+        setCode(value);
+    };
 
-  useEffect(initSocketSetting, []);
-  useEffect(maskingEvent, [code]);
+    useEffect(initSocketSetting, []);
+    useEffect(maskingEvent, [code]);
 
-  return (
-    <Editor
-      handleOnChange={handleOnChange}
-      code={code}
-    />
-  );
+    return (
+        <Editor
+            handleOnChange={handleOnChange}
+            code={code}
+        />
+    );
 }
 
 export default App;
